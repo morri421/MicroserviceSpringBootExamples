09ea9271d9d03352b118a35283cfacf27d15429f
Devon Morrison <devmorri421@gmail.com>
2023-07-04 20:00:20 -0400

    first commit

---
+++ src/test/java/com/example/microservicespringbootexamples/MicroserviceSpringBootExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicespringbootexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringBootExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
